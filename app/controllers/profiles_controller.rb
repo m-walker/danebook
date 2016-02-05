7746cfca74482097c15ab2feb97fbba923ad4951
Sia <siakaramalegos@gmail.com>
2016-02-05 16:34:10 -0600

    Activates profile show and edit views and actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,6 +8,12 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    if @profile.update(profile_params)
+      redirect_to @profile, notice: "Profile successfully updated!"
+    else
+      flash.now[:alert] = "There was a problem saving your profile."
+      render :edit
+    end
   end
 
   private
@@ -18,6 +24,6 @@ class ProfilesController < ApplicationController
   end
 
   def profile_params
-
+    params.require(:profile).permit(:dob, :college, :about, :words_to_live_by, :hometown, :phone, :current_city)
   end
 end
