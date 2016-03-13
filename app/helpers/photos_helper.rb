43633dc090b6c768abb286fa3c3198a83f380f32
Sia <siakaramalegos@gmail.com>
2016-03-13 16:13:17 -0500

    Adds cover photo and shows profile and cover in header

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,22 @@
 module PhotosHelper
+
+  def cover_photo(user)
+    if user.cover_photo
+      html = image_tag @user.cover_photo.photo.url(:cover), id: "cover-photo", class: "img-responsive", alt: "User's cover photo"
+    else
+      html = '<img src="http://placehold.it/1138x400" id="cover-photo" class="img-responsive", alt: "Default cover photo">'
+    end
+
+    html.html_safe
+  end
+
+  def profile_photo(user)
+    if user.cover_photo
+      html = image_tag @user.profile_photo.photo.url(:preview), id: "profile-photo", class: "img-responsive img-thumbnail"
+    else
+      html = '<img src="http://placehold.it/160x160" id="profile-photo" class="img-responsive img-thumbnail", alt: "Default profile photo">'
+    end
+
+    html.html_safe
+  end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,6 +5,6 @@ class Photo < ActiveRecord::Base
 
   # http://www.imagemagick.org/script/command-line-processing.php#geometry
   # timeline preserves width
-  has_attached_file :photo, styles: { timeline: "600>", thumb: "100x100#", profile_thumb: "80x80#", preview: "200x200#" }, default_url: "/images/missing_:style.jpg"
+  has_attached_file :photo, styles: { timeline: "600>", thumb: "100x100#", profile_thumb: "80x80#", preview: "200x200#", cover: "1138x400#" }, default_url: "/images/missing_:style.jpg"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
