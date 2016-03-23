2cb6a28c9b02ffe3fe35e20e0fde6ba2691aafba
Sia <siakaramalegos@gmail.com>
2016-03-23 12:06:54 -0500

    Adds recent activity friends to newsfeed

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+
+  def user_photo_column_width(photo_size)
+    photo_size == 'medium' ? "col-sm-3" : "col-sm-2"
+  end
+
+  def user_name_column_width(photo_size)
+    photo_size == 'medium' ? "col-sm-9" : "col-sm-10"
+  end
 end
